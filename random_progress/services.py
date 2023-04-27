d5270580f1b7a5ee573ceda1b11140a302ade0d9
Mike Sobolev <gemex18@gmail.com>
2023-04-27 16:49:55 +0300

    fix validate_none and function on services.py

--- random_progress/services.py
+++ random_progress/services.py
@@ -6,13 +6,13 @@ from dotenv import dotenv_values
 
 from random_progress.config import DEFAULT_CONFIG
 from random_progress.schemas import AppConfig
-from random_progress.validators import validate_data_types, validate_config
+from random_progress.validators import validate_none, validate_config
 
 
 def get_config():
-    config_date = _get_config_data()
+    config_data = _get_config_data()
     try:
-        app_config = AppConfig(**config_date)
+        app_config = AppConfig(**config_data)
         validate_config(app_config)
         return app_config
     except TypeError as err:
@@ -20,38 +20,38 @@ def get_config():
 
 
 def _get_config_data():
-    env_config = validate_data_types(_get_env_config())
-    json_config = validate_data_types(_get_json_config())
-    cli_config = validate_data_types(_get_cli_config())
+    env_config = validate_none(_get_env_config())
+    json_config = validate_none(_get_json_config())
+    cli_config = validate_none(_get_cli_config())
     return {**DEFAULT_CONFIG, **env_config, **json_config, **cli_config}
 
 
 def _get_cli_config():
-    args_params = parsers()
-    argses = {
+    args_params = get_args()
+    sleep_and_number_args = {
         "min_number": args_params.min_number,
         "max_number": args_params.max_number,
         "sleep_time": args_params.sleep_time,
     }
-    return argses
+    return sleep_and_number_args
 
 
 def _get_json_config():
-    path_arg = parsers()
+    path_args = get_args()
     try:
-        with open(path_arg.path) as file:
+        with open(path_args.path) as file:
             config_file = json.loads(file.read())
     except (FileNotFoundError, JSONDecodeError):
         config_file = {}
     return config_file
 
 
-def parsers():
+def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-min_number", type=float, action="store", help="write float")
-    parser.add_argument("-max_number", type=float, action="store", help="write float")
-    parser.add_argument("-sleep_time", type=float, action="store", help="write float")
-    parser.add_argument("-path", default="time_sleeping.json")
+    parser.add_argument("-min_number", type=float, action="store", help="write min number")
+    parser.add_argument("-max_number", type=float, action="store", help="write max number")
+    parser.add_argument("-sleep_time", type=float, action="store", help="write sleep time  ")
+    parser.add_argument("-path", type=str, default="time_sleeping.json", help="write path to the file JSON")
 
     args = parser.parse_args()
     return args
--- random_progress/validators.py
+++ random_progress/validators.py
@@ -1,29 +1,25 @@
 from random_progress.schemas import AppConfig
 
 
+def validate_none(config: dict):
+    dict_filter_none = {k: v for k, v in config.items() if v is not None}
+    return dict_filter_none
+
+
 def validate_config(config: AppConfig):
     _validate_numbers_non_negative(config)
     _validate_numbers(config)
+    _validate_data_types(config)
 
 
-def validate_data_types(config: dict):
-    def _validate_none(config_dict: dict):
-        dict_filter_none = dict()
-        for k, v in config_dict.items():
-            if v is not None:
-                dict_filter_none[k] = v
-        return dict_filter_none
-
-    def _validate_type(start_dict: dict):
-        final_dict = dict()
-        for k, v in start_dict.items():
-            try:
-                final_dict[k] = float(v)
-            except ValueError:
-                raise ValueError("argument should be float")
-        return final_dict
-
-    return _validate_type(_validate_none(config))
+def _validate_data_types(config: AppConfig):
+    final_dict = dict()
+    for k, v in config.__dict__.items():
+        try:
+            final_dict[k] = float(v)
+        except ValueError:
+            raise ValueError("argument should be float")
+    return final_dict
 
 
 def _validate_numbers_non_negative(app: AppConfig):
